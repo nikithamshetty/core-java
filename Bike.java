2eb388946cff3d8caa56b12aae552f3162d9c430
nikithamshetty <nikithamshetty13@gmail.com>
2023-10-02 20:22:37 +0530

    updated if

--- Bike.java
+++ Bike.java
@@ -25,12 +25,19 @@ this.exhaust=exhaust;
 }
 public static void main(String[] args)
 {
-Bike B1=new Bike("xblade",160,"fibre","grey","mono","single channel","Honda",140000,"dual","dual barrel");
-System.out.println(B1.price);
+Bike B1=null;
+if(B1!=null)
+{
 Bike B2=new Bike("passion",110,"metal","black","dual","drum break","Hero",90000,"dual","single exhaust");
 System.out.println(B2.Model);
 Bike B3=new Bike("Hunter",350,"metal","dapper white","mono","dual channel","Royal enfield",250000,"dual","single exhaust");
 System.out.println(B3.company);
 }
+else
+{
+System.out.println("Exception created");
 }
+}
+}
+
 
