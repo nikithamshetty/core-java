585760f81575e37d0345bdb4172eb99c56cb641f
nikithamshetty <nikithamshetty13@gmail.com>
2023-10-04 12:45:41 +0530

    object ideas

---
+++ Television.java
@@ -0,0 +1,20 @@
+public class Television{
+
+String name;
+int motherBoard;
+int ports;
+int buttons;
+
+static void toWatch()
+{
+}
+static void toPlay()
+{
+}
+static void forTimePass()
+{
+}
+public static void main(String[] args)
+{
+}
+}
