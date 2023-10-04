244b5d665c199f9d2dfe60351bd5f175008fefb7
nikithamshetty <nikithamshetty13@gmail.com>
2023-10-04 12:43:14 +0530

    object ideas

---
+++ CupBoard.java
@@ -0,0 +1,20 @@
+public class CupBoard{
+
+String wood;
+int mirror;
+int keys;
+int handel;
+
+static void forStoring()
+{
+}
+static void toSafeKeepThings()
+{
+}
+static void canLock()
+{
+}
+public static void main(String[] args)
+{
+}
+}
