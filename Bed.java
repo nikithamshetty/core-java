f90e745cba922d0d89dc3fc7ca7c6b508ff4dabe
nikithamshetty <nikithamshetty13@gmail.com>
2023-10-04 12:42:17 +0530

    object ideas

---
+++ Bed.java
@@ -0,0 +1,20 @@
+public class Bed{
+
+String wood;
+int quality;
+String design;
+String color;
+
+static void forRest()
+{
+}
+static void toSleep()
+{
+}
+static void toSeat()
+{
+}
+public static void main(String[] args)
+{
+}
+}
