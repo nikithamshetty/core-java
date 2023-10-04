e1ed5cce019096769b75180d814ccf79adc77a3c
nikithamshetty <nikithamshetty13@gmail.com>
2023-10-04 12:44:59 +0530

    object ideas

---
+++ Stove.java
@@ -0,0 +1,20 @@
+public class Stove{
+
+int burner;
+int stand;
+int nobs;
+String gaspipe;
+
+static void produceFire()
+{
+}
+static void heatFood()
+{
+}
+static void prepareFood()
+{
+}
+public static void main(String[] args)
+{
+}
+}
