258aafb8d96d2f845ef3d5e4055dac6548d0becb
nikithamshetty <nikithamshetty13@gmail.com>
2023-10-04 12:44:15 +0530

    object ideas

---
+++ Shoe.java
@@ -0,0 +1,20 @@
+public class Shoe{
+
+String quality;
+int price;
+String design;
+String lacecolor;
+
+static void toProtectFoot()
+{
+}
+static void towalk()
+{
+}
+static void toCoverFeet()
+{
+}
+public static void main(String[] args)
+{
+}
+}
