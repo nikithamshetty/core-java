e1237668df08125e3e49b093eb18057fdb956603
nikithamshetty <nikithamshetty13@gmail.com>
2023-10-06 10:17:32 +0530

    updated task

---
+++ RandomThingsUses.java
@@ -0,0 +1,138 @@
+public class RandomThingsUses{
+
+public static void main(String[] args)
+	{	
+		// using literals
+		String cupboard="To Store Things";
+		String bag="To carry Things ";
+		String glass="To Drink";
+		String spoon="To Eat";
+		String tv="For Entertainment";
+		String bed="To Sleep";
+		String laptop="For Working";
+		String paper="To Write";
+		String blanket="Keep us Warm";
+		String door="To Protect House";
+
+		String dress="To Wear";
+		String plate="To Eat food ";
+		String sofa="To Sit";
+		String mobile="To entertainment";
+		String shoe="Keep feet away from dirt";
+		String bucket="Fill the water ";
+		String curtain="To avoid sunrays";
+		String charger="For charging mobile";
+		String watch="It shows time";
+		String sink="To wash hands";
+
+		String solar="Provide hot water ";
+		String food="Dosa ";
+		String hanger="To hang the clothes";
+		String bus="To travel";
+		String webLanguages="HTML and CSS";
+		String mirror="To look ourself";
+		String projector="For presentation";
+		String shoerack="To organize shoes";
+		String lift="Can travel in between floors";
+		String pillow="To rest";
+
+        //reassigning values to existing variables
+		bag="It comes in variety";
+		glass="It can break easily";
+		tv="For timepass";
+		bed="To laydown";
+		door="made of wood";
+
+		dress="Has lot of designs";
+		plate="It can be metal and paper plates";
+		mobile="Has various brands";
+		charger="Every mobile needs charger";
+		watch="now a days smart watch are popular ";
+
+		solar="Only on sunny days";
+		hanger="Comes in different material";
+		mirror="Is made of glass";
+		lift="It can be dangerous";
+		pillow="For our comfort";
+
+
+		//using new keyword		
+                String flowers= new String("Can be used for decoration");
+		String water= new String("For thirst");
+		String vase= new String("To decorate with flowers");
+		String bulb= new String("for light");
+		String torch= new String("Can be portable");
+		String belt= new String("for pant safty");
+		String hook= new String("To hung things");
+		String rope= new String("To tie");
+		String cardboard= new String("For paper support");
+		String clips= new String("Support for washed clothes");
+
+		 String mouse= new String("For laptop");
+		String glitter= new String("To decorate the things");
+		String kerchief= new String("To swipe sweat");
+		String washingMachine= new String("To wash clothes");
+		String ribbon= new String("To tie hairs");
+		String cycle= new String("Good exercise for body");
+		String fan= new String("Provides air");
+		String ac= new String("It consume water");
+		String craddle= new String("sleeping chair for baby");
+		String gloves= new String("Keep hands in warm");
+
+		String bottle= new String("To store water");
+		String switches= new String("For onn and off");
+		String screwDriver= new String("To fix things");
+		String bike= new String("For transportation");
+		String aeroplane= new String("Flies in air");
+		String python= new String("Its a programming language");
+		String fish= new String("Stays in water");
+		String lion= new String("King of the jungle");
+		String tiger= new String("National animal");
+		String soap= new String("To clean ourself");
+
+
+                flowers="Rose, Jasmine";
+		water="To bathe";
+		vase="To house look";
+		torch="Used outside";
+		belt="For style";
+
+		mouse="Its an animal";
+		glitter="Shiny particle";
+		washingMachine="It consumes lot of water";
+		ribbon="Comes in variety of color";
+		cycle="To travel";
+
+		bottle="It has various litre options";
+		switches="Every electronic things has switch";
+		python="vicious snake";
+		fish="Cant survive in air";
+		soap="It forms foam";
+
+                System.out.println(cupboard==cupboard);
+		System.out.println(bag==glass);
+		System.out.println(spoon==dress);
+		System.out.println(mobile==charger);
+		System.out.println(door==door);
+		
+		System.out.println(sink==watch);
+		System.out.println(tv==tv);
+		System.out.println(bed==bus);
+		System.out.println(curtain==bucket);
+		System.out.println(solar==solar);
+		
+		System.out.println(lion==soap);
+		System.out.println(flowers==vase);
+		System.out.println(ribbon==ribbon);
+		System.out.println(bike==switches);
+		System.out.println(aeroplane==python);
+		
+		System.out.println(glass==craddle);
+		System.out.println(sofa==cardboard);
+		System.out.println(watch==glitter);
+		System.out.println(sink==hook);
+		System.out.println(pillow==rope);
+
+		
+}
+}
